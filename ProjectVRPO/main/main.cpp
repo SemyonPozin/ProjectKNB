2546bf5e9d2558c9d1418889840ef9814a95e86c
SemyonPozin <154556698+SemyonPozin@users.noreply.github.com>
2024-01-11 18:58:05 +0300

    1 version

---
+++ ProjectVRPO/main/main.cpp
@@ -0,0 +1,50 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+    srand(time(NULL));
+    while (true) {
+        setlocale(LC_CTYPE, "Russian");
+        int userChoose, compChooseKNB, userChooseKNB;
+        cout << "Выберите вариант использования программы." << endl;
+        cout << "1-Играть" << endl;
+        cout << "2-Выйти" << endl;
+        cin >> userChoose;
+        switch (userChoose)
+        {
+        case 1:
+            cout << "Выберите камень(1), ножницы(2) или бумагу(3)."<<endl;
+            cout << "Чтобы выйти в меню выберите 4." << endl;
+            while (true) {
+                compChooseKNB = rand() % 3 + 1;
+                cin >> userChooseKNB;
+                if ((userChooseKNB != 1) && (userChooseKNB != 2) && (userChooseKNB != 3) && (userChooseKNB != 4)) {
+                    cout << "Неправильный выбор!" << endl;
+                    break;
+                }
+                if ((compChooseKNB == 1 && userChooseKNB == 2) || (compChooseKNB == 2 && userChooseKNB == 3) || (compChooseKNB == 3 && userChooseKNB == 1)) {
+                    cout << "Вы проиграли!" << endl;
+                }
+                else
+                    if ((userChooseKNB == 1 && compChooseKNB == 2) || (userChooseKNB == 2 && compChooseKNB == 3) || (userChooseKNB == 3 && compChooseKNB == 1)) {
+                        cout << "Вы выиграли!" << endl;
+                    }
+                    else
+                        if (userChooseKNB == compChooseKNB) {
+                            cout << "Ничья!" << endl;
+                        }
+                        else
+                            if (userChooseKNB == 4)
+                                break;
+            }
+            break;
+        case 2:
+            return 0;
+        default:
+            cout << "Ошибка! Неверный вариант использования" << endl;
+            break;
+        }
+    }
+}
+
