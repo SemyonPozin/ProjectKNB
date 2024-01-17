3e1c5d8177077ac104ba424914132ce62cf65dee
sl1ghtSava <155019300+sl1ghtSava@users.noreply.github.com>
2024-01-18 00:31:19 +0300

    Add files via upload

---
+++ vrpoProject/vrpoProject/writeScissorsLeft.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void writeScissorsLeft() {
+	setlocale(LC_CTYPE, "Russian");
+	char p, s;
+	s = ' ';
+	p = 'ñ';
+
+
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p;
+	cout << setw(10) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p << endl;
+
+	cout << setw(2) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(7) << setfill(s) << s;
+	cout << setw(2) << setfill(p) << p << endl;
+
+	cout << setw(2) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(5) << setfill(s) << s;
+	cout << setw(2) << setfill(p) << p << endl;
+
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p;
+	cout << setw(1) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(2) << setfill(s) << s;
+	cout << setw(2) << setfill(p) << p << endl;
+
+	cout << setw(7) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p << endl;
+
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p;
+	cout << setw(1) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(2) << setfill(s) << s;
+	cout << setw(2) << setfill(p) << p << endl;
+
+	cout << setw(2) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(5) << setfill(s) << s;
+	cout << setw(2) << setfill(p) << p << endl;
+
+	cout << setw(2) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(1) << setfill(p) << p;
+	cout << setw(7) << setfill(s) << s;
+	cout << setw(2) << setfill(p) << p << endl;
+
+	cout << setw(3) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p;
+	cout << setw(10) << setfill(s) << s;
+	cout << setw(3) << setfill(p) << p << endl;
+
+}
